24524e940c03106452027b1334375bf4e55eeb2b
rotem zion <ro.sh.zion@gmail.com>
2023-08-28 00:18:00 +0300

    aa

--- sercheprojects/settings.py
+++ sercheprojects/settings.py
@@ -133,6 +133,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/class_61/django/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
