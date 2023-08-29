d84696d7db8bedf0504771445a53edbc5f1103d5
rotem zion <ro.sh.zion@gmail.com>
2023-08-29 21:52:41 +0300

    a

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sercheprojects.settings")
+
+
+import django
+django.setup()
+
+
+# here you can use all django models/features etc
+
+
+# from my_app.models import *
+
+# ...
